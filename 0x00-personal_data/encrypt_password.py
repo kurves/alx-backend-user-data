fc9dc0d76537acecb18ec29000a5031045556fb2
kurves <kavssimply@gmail.com>
2024-09-01 06:10:14 +0000

    check if password is valid

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,10 +10,14 @@ def hash_password(password: str) -> bytes:
     """
     Hash a password with a random salt using bcrypt.
     """
-    # Generate a salt
     salt = bcrypt.gensalt()
 
-    # Hash the password with the generated salt
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
 
     return hashed_password
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validate that the provided password matches the hashed password.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
