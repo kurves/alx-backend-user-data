1f7c0ca9db7869b9407f4865a7a19e256206e6d2
kurves <kavssimply@gmail.com>
2024-09-03 08:12:54 +0000

    Define routes that dont need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,7 +17,21 @@ class Auth:
         """
         require authentication function
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        #normalize path
+        if path[-1] != '/':
+            path += '/'
+        for excluded_path in excluded_paths:
+            if excluded_path[-1] != '/':
+                excluded_path += '/'
+            if path == excluded_path:
+                return False
+
+    return True
+
 
     def authorization_header(self, request=None) -> str:
         """
