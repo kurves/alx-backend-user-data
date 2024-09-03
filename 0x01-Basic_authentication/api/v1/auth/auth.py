51b18de41aa9f8b54231d4cffc062d90bcfe457e
kurves <kavssimply@gmail.com>
2024-09-03 07:49:00 +0000

    authentication class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,26 +5,26 @@ Authentication module
 """
 
 from flask import request
+from typing import List, TypeVar
 
-
-"""
-Authrntication clss
-"""
 
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         require authentication function
         """
+        return False
 
 
     def authorization_header(self, request=None) -> str:
         """
         authorization header function
         """
+        return None
+
 
    def current_user(self, request=None) -> TypeVar('User'):
        """
-
-   
-
+       current user functiuon
+       """
+       return None
