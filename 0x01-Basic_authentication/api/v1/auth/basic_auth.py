92420aa045669ca8dc57525204e283de45ab6398
kurves <kavssimply@gmail.com>
2024-09-03 14:34:54 +0000

    decode base64 authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,7 +13,7 @@ class BasicAuth(Auth):
     """
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
         """
-        Extracts the Base64 part of the Authorization header for Basic Authentication
+        Extracts the BAse 64 Basic Authentication
         """
         if authorization_header is None:
             return None
@@ -22,3 +22,18 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[len("Basic "):]
+
+     def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """
+        Decodes a Base64 string to a UTF-8 string
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except Exception:
+            return None
