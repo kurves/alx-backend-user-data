7ba5ae9a194d5fff8e2a9e1518b7b1071ebf2dd0
kurves <kavssimply@gmail.com>
2024-09-03 16:04:52 +0000

    import required dependency

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,7 +6,8 @@ Basic Auth module
 
 from api.v1.auth.auth import Auth
 import base64
-from typing import Tuple, TypeVar
+from typing import Tuple, TypeVar, Optional
+from models.user import User
 
 
 class BasicAuth(Auth):
