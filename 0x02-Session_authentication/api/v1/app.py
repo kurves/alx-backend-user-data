a6841e0bccc2c2fc90809ad21e977e83c6312275
kurves <kavssimply@gmail.com>
2024-09-05 15:16:35 +0000

    session auth class

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -7,7 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-
+from api.v1.auth.session_auth import session_auth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -21,6 +21,9 @@ if AUTH_TYPE == "auth":
 if AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+if AUTH_TYPE == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.before_request
@@ -37,8 +40,9 @@ def before_request() -> None:
 
     if auth.authorization_header(request) is None:
         abort(401)
+    request.current_user = auth.current_user(request)
 
-    if auth.current_user(request) is None:
+    if request.current_user is None:
         abort(403)
 
 
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/vin/env python3
+
+"""
+Session Authentication Module
+"""
+
+from api/v1/auth import Auth
+
+class SessionAuth(Auth):
+    """
+    Session Auth class
+    """
+    pass
+
