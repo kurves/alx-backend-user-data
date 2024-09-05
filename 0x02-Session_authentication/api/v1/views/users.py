f878d256eb9f0ce77d7c70ec1f97f404f84fca93
kurves <kavssimply@gmail.com>
2024-09-05 13:45:07 +0000

    Endpoint to retrieve authenticated user

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -19,28 +19,38 @@ def view_all_users() -> str:
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def view_one_user(user_id: str = None) -> str:
     """ GET /api/v1/users/:id
-    Path parameter:
-      - User ID
     Return:
       - User object JSON represented
-      - 404 if the User ID doesn't exist
     """
     if user_id is None:
         abort(404)
     user = User.get(user_id)
     if user is None:
         abort(404)
-    return jsonify(user.to_json())
+    if user_id == "me":
+        if request.current_user is None:
+            abort (404)
+        return jsonify(request.current_user.dict())
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    return jsonify(user.to_dict())
 
 
-@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/users/me', methods=['GET'], strict_slashes=False)
+def get_me(user_id: str = None) -> str:
+    """
+    GET /users/me/
+    """
+    if request.current_user is None:
+        abort(403)
+    return jsonify(request.current_user.to_dict())
+
+
 def delete_user(user_id: str = None) -> str:
     """ DELETE /api/v1/users/:id
-    Path parameter:
-      - User ID
     Return:
       - empty JSON is the User has been correctly deleted
-      - 404 if the User ID doesn't exist
     """
     if user_id is None:
         abort(404)
