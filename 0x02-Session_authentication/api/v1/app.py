6b4508b05beda832916e514b4b50e2c03602097a
kurves <kavssimply@gmail.com>
2024-09-08 07:11:59 +0000

    Sessions in database

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -28,6 +28,10 @@ if auth_type == 'session_exp_auth':
     from api.v1.auth.session_exp_auth import SessionExpAuth
     auth = SessionExpAuth()
 
+if auth_type == 'session_db_auth':
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
+
 
 @app.before_request
 def before_request() -> None:
