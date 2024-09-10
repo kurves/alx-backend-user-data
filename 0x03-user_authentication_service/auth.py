2eda19817c195f9579f018d4675c006530bed9c1
kurves <kavssimply@gmail.com>
2024-09-10 17:11:37 +0000

    register user functionlaity

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,8 @@ hash password module
 """
 
 import bcrypt
+from user import User
+
 
 def _hash_password(password: str) -> bytes:
     """
@@ -14,3 +16,15 @@ def _hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     
     return hashed_password
+
+def register_user(self, email: str, password: str) -> User:
+        """
+        Registers a user with the provided email and password.
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = self._hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
