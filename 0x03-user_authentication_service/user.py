3f6b02e2cbd7cf547c522aef39a461d60ace7da5
kurves <kavssimply@gmail.com>
2024-09-10 15:25:30 +0000

    usre model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+User model module for storing user information.
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    """
+    User class that represents the 'users' table.
+    """
+
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(128), nullable=False)
+    hashed_password = Column(String(128), nullable=False)
+    session_id = Column(String(128), nullable=True)
+    reset_token = Column(String(128), nullable=True)
+
+    def __init__(self, email: str, hashed_password: str, session_id: str = None, reset_token: str = None):
+        """
+        Initialize a User instance.
+        """
+        self.email = email
+        self.hashed_password = hashed_password
+        self.session_id = session_id
+        self.reset_token = reset_token
+
