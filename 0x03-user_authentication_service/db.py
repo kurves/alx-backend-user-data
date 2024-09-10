9ce2ab8ce2045024832cd673fb1d0676a03a4987
kurves <kavssimply@gmail.com>
2024-09-10 16:15:00 +0000

    update user functionality

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -8,9 +8,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import InvalidRequestError
-
+from sqlalchemy.orm.exc import NoResultFound, InvalidRequestError
+from typing import Dict
 from user import Base
 from user import User
 
@@ -23,7 +22,7 @@ class DB:
         """
         Initialize a new DB instance and create the necessary tables.
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -60,3 +59,20 @@ class DB:
             raise NoResultFound("No user found with the specified attributes")
         except InvalidRequestError:
             raise InvalidRequestError("Invalid request, check the query arguments")
+
+     def update_user(self, user_id: int, **kwargs: Dict) -> None:
+        """
+        try:
+            user = self.find_user_by(id=user_id)
+
+            for key, value in kwargs.items():
+                if not hasattr(user, key):
+                    raise ValueError(f"Invalid attribute: {key}")
+                setattr(user, key, value)
+
+            self._session.commit()
+
+        except NoResultFound:
+            raise ValueError(f"No user found with id: {user_id}")
+        except InvalidRequestError as e:
+            raise ValueError(f"Error during update: {str(e)}")
