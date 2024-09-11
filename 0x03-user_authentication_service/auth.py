6055e3e5b115940cd2eb894fac0ccbe4d05c452f
kurves <kavssimply@gmail.com>
2024-09-11 15:54:30 +0000

    credentials validations

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -50,3 +50,15 @@ class Auth:
             return False
         
         return False
+
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Validates if the user login is correct.
+
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+        except Exception:
+            return False
